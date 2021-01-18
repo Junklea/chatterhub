dc92f8ce8a4df4f7fb9fe9ccd0b3ccaff8eb5db3
junklea <taricerkan@gmail.com>
2021-01-18 17:23:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# chatterhub
+
+- [ ] Auth
+- [ ] Simple UI
+- [ ] P2P Messaging System
+- [ ] Group System
---
+++ android/app/src/main/kotlin/com/example/chatterhub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatterhub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
